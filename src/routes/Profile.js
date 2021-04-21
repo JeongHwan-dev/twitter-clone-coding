d29708276662e87c055f70a9c6388cd9683daf31
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-04-21 17:25:29 +0900

    Add get my own tweets function

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { authService } from "fBase";
+import { authService, dbService } from "fBase";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
-    // history.push("/");
-    window.location.replace("/");
+    history.push("/");
+    // window.location.replace("/");
   };
+
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createAt", "desc")
+      .get();
+
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
